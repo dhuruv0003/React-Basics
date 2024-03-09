35f386d441ab74fb56fe6501d2dc1eedafae8821
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-09 13:40:31 +0530

    New Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import "./App.css"
+import "./App.css";
+import Title2 from "./Title.jsx";
 
-function Title() {
-  return <h1>Hello World</h1>;
-}
 function Description(){
   return <h2>This is description</h2>
 }
 
 function App() {
   return <div>
+    <Title2></Title2>
     <h1>This is my first app component</h1>
     <h3>Inside the app we have : </h3>
-    <Title/>
+    <Title2/>
     <Description/>
       </div>
 }
