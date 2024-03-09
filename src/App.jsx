6790d02a866f13ceedf6d9cb7b16e02635e583c9
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-10 00:42:47 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 // import {Title,NewTitle} from "./Title.jsx";
 import Title from "./Title.jsx"
+import Shit from "./shit.jsx"
 
 function Description() {
   return <h2 className="raju">This is description</h2>
@@ -12,6 +13,7 @@ function App() {
     <h1 className="raju">This is my first app component</h1>
     <h3>Inside the app we have : </h3>
     <Title />
+    <Shit/>
     <Description></Description>
   </>)
 }
