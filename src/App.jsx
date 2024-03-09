11f0fbde9740cfeda3a2dc777fa41031231ad64c
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-09 10:20:27 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css"
 
+function Title() {
+  return <h1>Hello World</h1>;
+}
+function Description(){
+  return <h2>This is description</h2>
+}
+
 function App() {
-  return (<button>
-    <h2>hello world</h2>
-  </button>
-  )
+  return <div>
+    <h1>This is my first app component</h1>
+    <h3>Inside the app we have : </h3>
+    <Title/>
+    <Description/>
+      </div>
 }
 
 export default App
