b237a3213961fd600bdd7fb4cccee04d2f17838f
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-10 00:06:26 +0530

    new commit

---
+++ src/Title.jsx
@@ -0,0 +1,9 @@
+function Title() {
+    return <h1>Hello World</h1>;
+}
+
+function NewTitle(){
+    return <h3>This is new Title for Hello World</h3>
+}
+// export {Title,NewTitle};
+export default Title
