bf209e2c84177be4baf111bbbffa505e34718013
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-10 02:47:13 +0530

    new commit

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,11 +1,16 @@
 import './Product.css'
-function Product({ title, price }) {
+function Product({ title, price,features=["hi","hello","kasie ho"] }) {
     return (
         <div className="Product">
             <h1>Product Title: {title}</h1>
-            <h3>Product Price : {price/2}</h3>
+            <h3>Product Price : {price}</h3>
+            <h2>{features.toString().split(",").join("->")}</h2>
         </div>
     );
 }
 
 export default Product
+
+// Note we can also set default var;ue for the props 
+
+// Product({ title="kuch ni", price =2})
--- src/Productab.jsx
+++ src/Productab.jsx
@@ -1,10 +1,14 @@
 import Product from "./Product"
+import './ProductTab.css'
 function ProductTab() {
+    let features1 = ["khao", "piyo", "aish", "karo"] // array as arguement
+    let features2 = { a: "mast", b: "Hai", c: "bro" }
     return (
-        <div className="">
-            <Product title="phone" price="20k"></Product>
-            <Product title ="car" price="7lakh"></Product>
-            <Product title="laptop" price={5800}></Product>
+
+        <div className="ProductTab">
+            <Product title="phone" price="20k" features={features2}></Product>
+            <Product title="car" price="7lakh" features={features1}></Product>
+            <Product title="laptop" price={5800} features={["rsju","Bangya"]}></Product>
         </div>
 
     )
